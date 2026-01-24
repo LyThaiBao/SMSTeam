e01eac02c2d11eda254b93e2b53a943f46def3c0
Thai Bao <baoly069@gmail.com>
2026-01-24 16:56:40 +0700

    add animation

--- src/app/(public)/_components/StudentFeature.tsx
+++ src/app/(public)/_components/StudentFeature.tsx
@@ -1,36 +1,48 @@
+"use client";
 import Image from "next/image";
-
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 export default function StudentFeature() {
+  useEffect(() => {
+    AOS.init({ duration: 500 });
+  }, []);
   return (
     <section className="text-black w-[100%] ">
       <h2 className="text-xl font-bold py-2">Chức Năng Sinh Viên</h2>
-      <ul className="grid gap-2 max-w-[90%] my-0 mx-auto  ">
-        <li className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-all ">
-          <Image
-            src={"/imgs/stu.png"}
-            width={70}
-            height={70}
-            alt="feature"
-          ></Image>
-          <p>Theo dõi quá trình, kết quả học tập</p>
+      <ul className="grid gap-2 max-w-[90%] my-0 mx-auto md:grid-cols-2 md:gap-4 ">
+        <li data-aos="fade-left">
+          <div className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-all ">
+            <Image
+              src={"/imgs/stu.png"}
+              width={70}
+              height={70}
+              alt="feature"
+            ></Image>
+            <p>Theo dõi quá trình, kết quả học tập</p>
+          </div>
         </li>
-        <li className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-all">
-          <Image
-            src={"/imgs/stu.png"}
-            width={70}
-            height={70}
-            alt="feature"
-          ></Image>
-          <p>Theo dõi quá trình, kết quả học tập</p>
+        <li data-aos="fade-right">
+          <div className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-all ">
+            <Image
+              src={"/imgs/stu.png"}
+              width={70}
+              height={70}
+              alt="feature"
+            ></Image>
+            <p>Theo dõi quá trình, kết quả học tập</p>
+          </div>
         </li>
-        <li className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-all">
-          <Image
-            src={"/imgs/stu.png"}
-            width={70}
-            height={70}
-            alt="feature"
-          ></Image>
-          <p>Theo dõi quá trình, kết quả học tập</p>
+        <li data-aos="fade-up">
+          <div className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-all ">
+            <Image
+              src={"/imgs/stu.png"}
+              width={70}
+              height={70}
+              alt="feature"
+            ></Image>
+            <p>Theo dõi quá trình, kết quả học tập</p>
+          </div>
         </li>
       </ul>
     </section>
--- src/app/(public)/_components/TeacherFeature.tsx
+++ src/app/(public)/_components/TeacherFeature.tsx
@@ -4,41 +4,46 @@ import Image from "next/image";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-useEffect(() => {
-  AOS.init({ duration: 800 });
-}, []);
-
 export default function TeacherFeature() {
+  useEffect(() => {
+    AOS.init({ duration: 500 });
+  }, []);
   return (
     <section className="text-black ">
       <h2 className="text-xl font-bold py-2">Chức Năng Giảng Viên</h2>
-      <ul className="grid gap-2 max-w-[90%] my-0 mx-auto ">
-        <li className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-all">
-          <Image
-            src={"/imgs/stu.png"}
-            width={70}
-            height={70}
-            alt="feature"
-          ></Image>
-          <p>Theo dõi quá trình, kết quả học tập</p>
+      <ul className="grid gap-2 max-w-[90%] my-0 mx-auto md:grid-cols-2 md:gap-4">
+        <li data-aos="fade-up">
+          <div className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-transform duration-500 ease-out ">
+            <Image
+              src={"/imgs/stu.png"}
+              width={70}
+              height={70}
+              alt="feature"
+            ></Image>
+            <p>Theo dõi quá trình, kết quả học tập</p>
+          </div>
         </li>
-        <li className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-all">
-          <Image
-            src={"/imgs/stu.png"}
-            width={70}
-            height={70}
-            alt="feature"
-          ></Image>
-          <p>Theo dõi quá trình, kết quả học tập</p>
+        <li data-aos="fade-up">
+          <div className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-transform duration-500 ease-out ">
+            <Image
+              src={"/imgs/stu.png"}
+              width={70}
+              height={70}
+              alt="feature"
+            ></Image>
+            <p>Theo dõi quá trình, kết quả học tập</p>
+          </div>
         </li>
-        <li className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-all ">
-          <Image
-            src={"/imgs/stu.png"}
-            width={70}
-            height={70}
-            alt="feature"
-          ></Image>
-          <p>Theo dõi quá trình, kết quả học tập</p>
+        <li data-aos="fade-up">
+          <div className="border-1 rounded-2xl flex items-center justify-between p-2 hover:scale-105 transition-transform duration-500 ease-out ">
+            <Image
+              src={"/imgs/stu.png"}
+              width={70}
+              height={70}
+              alt="feature"
+            ></Image>
+            <p>Theo dõi quá trình, kết quả học tập</p>
+          </div>
         </li>
       </ul>
     </section>
