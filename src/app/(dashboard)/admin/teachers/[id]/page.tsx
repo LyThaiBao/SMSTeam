5b235fe22d50102df0127e0c0fd7c16d02730550
timy <baoly069@gmail.com>
2026-03-22 19:26:39 +0700

    fix test

--- src/app/(dashboard)/admin/teachers/[id]/page.tsx
+++ src/app/(dashboard)/admin/teachers/[id]/page.tsx
@@ -19,7 +19,7 @@ export default async function TeacherDetailPage({
   }
   return (
     <div className="">
-      <DetailTeacher {...teacher.abc} />
+      <DetailTeacher {...teacher.DATA} />
     </div>
   );
 }
